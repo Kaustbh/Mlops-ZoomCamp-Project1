027ee1a6112998736905c9b3b99462f9b391733b
Kaustbh <desalekaustubh3@gmail.com>
2024-08-05 23:33:35 +0530

    Modified data ingestion

--- ingestion/UnZipRaw.py
+++ ingestion/UnZipRaw.py
@@ -1,8 +1,9 @@
 import os.path
 from zipfile import ZipFile
 
-ZIP_FILE = '/home/kaustubh/mlops_zoomcamp/final_project/project_1/data/extended_crab_age_pred.zip'
-RAW_FOLDER = '/home/kaustubh/mlops_zoomcamp/final_project/project_1/data'
+ZIP_FILE = "/home/kaustubh/mlops_zoomcamp/final_project/project_1/data/extended_crab_age_pred.zip"
+RAW_FOLDER = "/home/kaustubh/mlops_zoomcamp/final_project/project_1/data"
+
 
 def unzip_raw_data(zip_file=ZIP_FILE, extract_to=RAW_FOLDER):
     # Check if the zip file exists
@@ -10,9 +11,9 @@ def unzip_raw_data(zip_file=ZIP_FILE, extract_to=RAW_FOLDER):
         zipfile = ZipFile(zip_file)
         zipfile.extractall(path=extract_to)
 
+
 # Call the function
 # unzip_raw_data()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unzip_raw_data()
-    
